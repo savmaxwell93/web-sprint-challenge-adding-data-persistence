dff940bde7aad40c16122162500d5fee63134cce
Savannah <savmaxwell@gmail.com>
2022-03-25 16:21:51 -0600

    done

--- api/task/middleware.js
+++ api/task/middleware.js
@@ -13,26 +13,4 @@ const checkTaskPayload = async (req, res, next) => {
     }
 }
 
-const checkProjectID = async (req, res, next) => {
-    try {
-        const existing = await db('tasks')
-            .where('project_id', req.params.project_id)
-            .first()
-
-        if(!existing) {
-            next({
-                status: 404,
-                message: `project with project_id ${req.params.project_id} not found`
-            })
-        } else {
-            next()
-        }
-    } catch (err) {
-        next(err)
-    }
-}
-
-module.exports = {
-    checkTaskPayload,
-    checkProjectID
-}
+module.exports = { checkTaskPayload }
--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Task = require('./model');
-const { checkTaskPayload, checkProjectID } = require('./middleware');
+const { checkTaskPayload } = require('./middleware');
 
 router.get('/', async (req, res, next) => {
     try {
